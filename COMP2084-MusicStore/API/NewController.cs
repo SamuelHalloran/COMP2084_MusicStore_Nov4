6101bcfb2c7ae250d4a931fcfbb18214f9dedd39
Samuel Halloran <Samuel.Halloran@expogain.com>
2019-11-25 15:28:20 -0500

    nov 25 part 2

--- COMP2084-MusicStore/API/NewController.cs
+++ COMP2084-MusicStore/API/NewController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using COMP2084_MusicStore.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,18 +12,30 @@ namespace COMP2084_MusicStore.API
     [ApiController]
     public class NewController : Controller
     {
+        private readonly MusicStoreContext _context;
+
+        public NewController(MusicStoreContext context)
+        {
+            _context = context;
+        }
+
+
         // GET: api/New
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IActionResult Get()
         {
-            return new string[] { "value1", "value2" };
+            var songs = _context.Song.ToList();
+
+            return Json(songs);
         }
 
         // GET: api/New/5
         [HttpGet("{id}", Name = "Get")]
         public IActionResult Get(int id)
         {
-            return Json(new { A = "B" });
+            var song = _context.Song.Where(x => x.SongId == id).SingleOrDefault();
+
+            return Json(song);
         }
 
         // POST: api/New
